987c40f05aa2b3bda74bf499094e3f34c2a9a486
Dxdnsxsty <xsty_dxdns@163.com>
2026-05-05 14:32:45 +0800

    修改硬编码

--- baselines/defense/DUP_Net/DUP_Net.py
+++ baselines/defense/DUP_Net/DUP_Net.py
@@ -1,6 +1,4 @@
 import os
-import numpy as np
-
 import torch
 import torch.nn as nn
 
@@ -9,26 +7,34 @@ from ..drop_points import SORDefense
 
 
 class DUPNet(nn.Module):
-
-    def __init__(self, sor_k=2, sor_alpha=1.1,
-                 npoint=1024, up_ratio=4):
+    def __init__(self, sor_k=2, sor_alpha=1.1, npoint=1024, up_ratio=4):
         super(DUPNet, self).__init__()
 
         self.npoint = npoint
         self.sor = SORDefense(k=sor_k, alpha=sor_alpha)
-        self.pu_net = PUNet(npoint=self.npoint, up_ratio=up_ratio,
-                            use_normal=False, use_bn=False, use_res=False)
-        self.pu_net.load_state_dict(torch.load('/public/huangqidong/3D_point_cloud/CVPR2022/baselines/defense/DUP_Net/pu-in_1024-up_4.pth'))
+
+        self.pu_net = PUNet(
+            npoint=self.npoint,
+            up_ratio=up_ratio,
+            use_normal=False,
+            use_bn=False,
+            use_res=False,
+        )
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        weight_path = os.path.join(current_dir, "pu-in_1024-up_4.pth")
+
+        self.pu_net.load_state_dict(
+            torch.load(weight_path, map_location="cuda" if torch.cuda.is_available() else "cpu")
+        )
+
         self.pu_net = self.pu_net.cuda()
         self.pu_net.eval()
 
     def forward(self, x):
         with torch.enable_grad():
-            x = self.sor(x)  # a list of pc
+            x = self.sor(x)
             x = x.transpose(1, 2)
-            x = self.pu_net(x)  # [B, N * r, 3]
+            x = self.pu_net(x)
             x = x.transpose(1, 2)
         return x
-
-
-
